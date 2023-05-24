e3548a3f6e66aacc31ed6b8958451e404ddd013e
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2023-05-24 16:34:49 +0300

    Update quiz.js

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,21 +1,30 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import Question from "../components/Question";
 import ExerciseList from "../components/ExerciseList";
-import polygon from "../public/Polygon 1.svg";
+import polygon from "../public/kirpich.jpg";
 import Image from "next/image";
 import { Input } from "postcss";
+import cls from 'classnames'
+
 
 export function getServerSideProps() {
   const exercises = [
-    { id: 0, title: "Каркасный дом" },
-    { id: 1, title: "Кирпичный дом" },
-    { id: 2, title: "Дом из газобетона" },
-    { id: 3, title: "Дом из бруса" },
-    { id: 4, title: "Баня" },
-    { id: 5, title: "Комерческое помещение" },
-    { id: 6, title: "Другое" },
+    { id: 2, title: "Дом из газобетона", imageUrl: "/../public/beton.jpg" },
+    { id: 1, title: "Кирпичный дом", imageUrl: "/../public/kirpich.jpg" },
+    { id: 1, title: "Деревянный дом", imageUrl: "/../public/wood.jpg" },
+    { id: 1, title: "Мелкое строение", imageUrl: "/../public/small.jpg" },
+    // { id: 0, title: "Каркасный дом" },
+    // { id: 3, title: "Дом из бруса" },
+    // { id: 4, title: "Баня" },
+    {
+      id: 5,
+      title: "Коммерческая недвижимость",
+      imageUrl: "/../public/commerce.jpg",
+    },
+    { id: 5, title: "Другое", imageUrl: "/../public/other.jpg" },
+    // { id: 6, title: "Другое" },
   ];
 
   return {
@@ -30,7 +39,7 @@ export function getQuestions(exerciseId) {
     {
       id: 0,
       exerciseId: 0,
-      question: "СКОЛЬКО ЭТАЖЕЙ ПЛАНИРУЕТСЯ?",
+      question: "Сколько этажей планируется?",
       answers: [
         "1 этаж",
         "2 этажа",
@@ -44,7 +53,7 @@ export function getQuestions(exerciseId) {
     {
       id: 1,
       exerciseId: 0,
-      question: "КАКОЙ ТИП ФУНДАМЕНТА РАССМАТРИВАЕТЕ?",
+      question: "Какой тип фундамента рассматриваете?",
       answers: [
         "Монолитная плита",
         "Утепленная плита с ростверком",
@@ -132,6 +141,14 @@ export default function Home({ exercises }) {
   const [state, setState] = useState(initialState);
   const { isExerciseShown, questions, isExerciseDone, score } = state;
 
+  const [isMobile, setIsMobile] = useState();
+
+  useEffect(() => {
+    if (window) {
+      setIsMobile(window.innerWidth < 800);
+    }
+  }, []);
+
   const showExercise = (id) => {
     setState({
       ...state,
@@ -157,91 +174,86 @@ export default function Home({ exercises }) {
         <title>Quiz</title>
         <meta name="description" content="Quiz app in next js" />
       </Head>
-      <div>
-        <main className="static block w-full flex flex-col items-center justify-center pt-20 px-10 ">
-          <div className="z-10">
-            {submitted ? (
-              <div className="py-[20%] px-[10%]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </div>
-            ) : !isExerciseShown ? (
-              <ExerciseList exercises={exercises} func={showExercise} />
-            ) : isExerciseDone ? (
-              <div>
-                <form className="flex flex-col space-y-10 py-[20%]">
-                  <div className="flex flex-col">
-                    <label htmlFor="name">
-                      Площать пильного фундамента в кв/м
-                    </label>
-                    <input
-                      className="rounded-md bg-white p-2 border border-gray"
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                      name="name"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="message">Телефон</label>
-                    <input
-                      type="tel"
-                      className="rounded-md bg-white p-2 border border-gray"
-                      onChange={(e) => {
-                        setMessage(e.target.value);
-                      }}
-                      name="message"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="email">Почта</label>
-                    <input
-                      type="email"
-                      className="rounded-md bg-white p-2 border border-gray"
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                      name="email"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    onClick={(e) => {
-                      handleSubmit(e);
+
+      <main className={cls(" w-full flex bg-black text-white grow min-h-[100%] flex-col items-center justify-center pt-20 sm:pt-[40px] px-5 pb-20 ", {
+        '!items-start !justify-start ': isMobile
+      })}>
+        <>
+          {submitted ? (
+            <div className="py-[20%] px-[10%]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          ) : !isExerciseShown ? (
+            <ExerciseList
+              exercises={exercises}
+              func={showExercise}
+              isMobile={isMobile}
+            />
+          ) : isExerciseDone ? (
+            <div>
+              <form className="flex flex-col  space-y-10 py-[20%]">
+                <div className="flex flex-col">
+                  <label htmlFor="name">
+                    Площать пильного фундамента в кв/м
+                  </label>
+                  <input
+                    className="rounded-md  text-black bg-white p-2 border border-gray"
+                    onChange={(e) => {
+                      setName(e.target.value);
                     }}
-                  >
-                    Submit
-                  </button>
-                </form>
-              </div>
-            ) : (
-              <Question
-                questions={questions}
-                hideExercise={hideExercise}
-                finishTest={finishTest}
-                setAllAnswers={setAllAnswers}
-                allAnswers={allAnswers}
-              />
-            )}
-          </div>
-          <Image
-            src={polygon}
-            className="absolute top-0 right-0 z-[-1] object-contain rotate-180  max-w-[50%] max-h-[50%]"
-            alt=""
-          />
-        </main>
-        <Image
-          className="mt-auto  object-contain max-w-[50%] max-h-[50%]"
-          src={polygon}
-          alt=""
-        />
-      </div>
+                    name="name"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="message">Телефон</label>
+                  <input
+                    type="tel"
+                    className="rounded-md text-black bg-white p-2 border border-gray"
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
+                    name="message"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="email">Почта</label>
+                  <input
+                    type="email"
+                    className="rounded-md text-black bg-white p-2 border border-gray"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    name="email"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  onClick={(e) => {
+                    handleSubmit(e);
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Question
+              questions={questions}
+              hideExercise={hideExercise}
+              finishTest={finishTest}
+              setAllAnswers={setAllAnswers}
+              allAnswers={allAnswers}
+              isMobile={isMobile}
+            />
+          )}
+        </>
+      </main>
     </>
   );
 }
-
