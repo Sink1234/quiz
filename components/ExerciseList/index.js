29540d64f493007cef243e2b96248cda106793ac
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2023-05-24 17:03:54 +0300

    Update index.js

--- components/ExerciseList/index.js
+++ components/ExerciseList/index.js
@@ -5,7 +5,7 @@ export default function ExerciseList({ exercises, isMobile, func }) {
   return (
     <div className="max-w-[1000px] mx-auto">
       <h5 className={cls("text-6xl font-bold uppercase", {
-        'text-[32px]' : isMobile
+        '!text-[32px]' : isMobile
       })}>шаг 1</h5>
       <p className="text-[#6D6D6D] text-[20px] mb-10 mt-1 font-medium">
         Для какого строения требуется фундамент?
@@ -20,7 +20,7 @@ export default function ExerciseList({ exercises, isMobile, func }) {
             className={cls(
               "hover:text-blue-900 cursor-pointer hover:text-green-900 transform transition hover:scale-105 flex items-center flex-col  mr-[50px] mb-[30px] max-w-[190px]",
               {
-                "space-x-[10px] max-w-[150px] mr-0": isMobile,
+                "!space-x-[10px] !max-w-[150px] !mr-0": isMobile,
               }
             )}
             onClick={() => func(exercise.id)}
