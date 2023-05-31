a427638cd9fea8310ab6d7485a3ae0aebbc5e8e9
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2023-05-31 21:56:57 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Index() {
       </Head>
 
       <div className="relative min-h-screen flex flex-col">
+        <div className="full-v"></div>
         <Image
           src={black}
           className=" z-[-1] absolute sm:min-h-[570px] object-cover top-0 left-0 w-full z-10"
