5d834a1d9db764c89d6c47db3236bc5e346b2b8f
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2023-06-17 08:56:05 +0300

    Update index.js answers

--- components/Question/Answers/Answer/index.js
+++ components/Question/Answers/Answer/index.js
@@ -4,7 +4,7 @@ export default function Answer(props) {
   return (
     <li>
       <button
-        className="p-1 sm:p-0 mb-5 flex items-center text-[#6D6D6D] text-start gap-2 hover:text-green"
+        className="p-1 sm:p-0 mb-5 flex items-center text-[#fff] text-start gap-2 hover:text-green"
         onClick={() => answerQuestion(answerValue, answerText)}
       >
         <span className="mr-2">{icon}</span>
